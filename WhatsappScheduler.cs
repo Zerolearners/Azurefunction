4222432f02989432f038cb1448b1ed0d8bbad0c6
abel-sajin <abel-sajin@chavara.local>
2025-05-02 17:50:21 +0530

    ADDED

--- WhatsappScheduler.cs
+++ WhatsappScheduler.cs
@@ -20,7 +20,7 @@ namespace Company.Function
             _setting = setting;
         }
 
-        // [Function("WhatsappScheduler")]  test
+        // [Function("WhatsappScheduler")]  test1
         public async Task Run([TimerTrigger("0 */2 * * * *")] TimerInfo myTimer)
         {
             _logger.LogInformation($"WhatsappScheduler function executed at: {DateTime.Now}");
